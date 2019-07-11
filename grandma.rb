1f0ac5aa5cb6e8065598dd9891e899b54ef71da3
Chantel Lowe <chantellowe17@gmail.com>
2019-07-11 00:21:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-
+input_expression
 def speak_to_grandma(expression)
   if expression.upcase == nil
     return "HUH?! SPEAK UP, SONNY!"
